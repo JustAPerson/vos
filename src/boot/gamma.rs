384cbd83f722354932a2a168a623e8d322e50999
Jason Priest <jpriest128@gmail.com>
2015-08-13 00:28:07 -0500

    Test libdisk
    Should probably actually execute the kernel.img, but
    that can be done later.

--- src/boot/gamma.rs
+++ src/boot/gamma.rs
@@ -1,6 +1,5 @@
 #![crate_type="lib"]
-#![feature(no_std, lang_items, asm, intrinsics)]
-#![no_std]
+#![feature(asm, intrinsics)]
 
 #[no_mangle]
 pub fn rust_boot() {
@@ -8,33 +7,16 @@ pub fn rust_boot() {
         bios::print_string("Hello from Rust! Yay!!");
     }
 
+    let mut disk = disk::BiosDisk::new();
+    let mut fs = disk::mount(&mut disk, 0).unwrap(); // TODO mount correct partition
+    // TODO unwrap()????
+
+    fs.read_file("/kernel.img".as_ref());
+
     // prevent CPU executing past this point
     // anything beyond this is likely an invalid instruction sequence
     loop {}
 }
 
 mod bios;
-// extern disk;
-
-// all of this is required due to no libstd
-pub mod lang {
-    #[no_mangle]
-    pub fn __morestack() {}
-
-    #[lang="panic_fmt"]
-    fn panic_fmt() {}
-    #[lang="stack_exhausted"]
-    fn stack_exhausted() {}
-    #[lang="eh_personality"]
-    fn eh_personality() {}
-
-    // below no longer necessary once we get libcore
-    #[lang="sized"]
-    trait Sized {}
-    #[lang="copy"]
-    trait Copy {}
-    #[lang="sync"]
-    trait Sync {}
-
-    impl Sync for &'static str {}
-}
+extern crate disk;
